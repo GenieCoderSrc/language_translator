d25579befc472ae6e1789cbc663f69144b465420
SuraiyaKhatunItHome <suraiya.it.home@gmail.com>
2025-06-25 22:40:05 +0600

    Initial support

--- lib/extensions/translate_string_extension.dart
+++ lib/extensions/translate_string_extension.dart
@@ -1,7 +1,7 @@
 import 'package:dart_data_type_parser/dart_data_type_parsers/string_extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:language_translator/utils/translate.dart';
-import 'package:navigation_without_context/navigator/navigator.dart';
+import 'package:navigation_wihout_context/di/register_navigator_get_it_di.dart';
 
 extension TranslateStringExtension on String {
   String translateTxt(BuildContext context) =>
